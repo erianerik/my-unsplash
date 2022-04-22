640233d53d9559aa4fc914e1ab194059f51265da
Erian Erik <erianerik@gmail.com>
2022-04-21 21:55:29 -0300

    chore: adicionando componente de adicionar imagem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ImagesComponent } from './components/images/images.component';
+import { AddImageComponent } from './components/add-image/add-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ImagesComponent
+    ImagesComponent,
+    AddImageComponent
   ],
   imports: [
     BrowserModule,
